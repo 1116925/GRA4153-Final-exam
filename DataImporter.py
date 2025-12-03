5c28b117699010b089126456f75df66181eec588
1116925 <s2512542@bi.no>
2025-12-03 14:14:50 +0100

    Update in formating of the time series and added the plot

--- DataImporter.py
+++ DataImporter.py
@@ -6,10 +6,11 @@ import pandas as pd
 class Data:
     def __init__(self, target_CSV: str):
         self._raw_data = pd.read_csv(target_CSV)
-        self._sample = self._raw_data
 
         # Ensure observation_date is datetime
         self._raw_data['observation_date'] = pd.to_datetime(self._raw_data['observation_date'])
+        self._raw_data.set_index('observation_date', inplace=True)
+        self._sample = self._raw_data
         
     def set_sample_range(self, start_date: str, end_date: str):
         '''
@@ -20,8 +21,7 @@ class Data:
         :param end_date: The end date for filtering
         :type end_date: str
         '''
-        mask = (self._raw_data['observation_date'] >= start_date) & (self._raw_data['observation_date'] <= end_date)
-        self._sample = self._raw_data.loc[mask] 
+        self._sample = self._raw_data.loc[start_date:end_date]
         
     @property
     def sample(self):
